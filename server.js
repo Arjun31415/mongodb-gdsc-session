2f81718b63b67fbb27ad9423f2a6fcc6e41dfb66
MohammadShaad <mohammadshaadshaikh@gmail.com>
2023-05-10 23:02:53 +0530

    Connected MongoDB Server

--- server.js
+++ server.js
@@ -13,6 +13,24 @@ import cors from "cors";
 import morgan from "morgan";
 import MainController from "./controllers/MainController.js";
 import APIController from "./controllers/APIController.js";
+import { MongoClient } from 'mongodb';
+import assert from 'assert';
+
+
+// *********************************
+// MongoDB Variable Declarations
+// *********************************
+
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'todo';
+
+
+// Create a new MongoClient
+const client = new MongoClient(url);
 
 // *********************************
 // Global Variables & Controller Instantiation
@@ -50,7 +68,7 @@ APIRoutes.use(cors());
 // *********************************
 // Routes that Render Pages with EJS
 // *********************************
-MainRoutes.get("/", mainController.index); // "/"
+MainRoutes.get("/", mainController.index)// "/"
 MainRoutes.get("/error", mainController.error);
 
 // *********************************
@@ -60,7 +78,33 @@ APIRoutes.get("/", apiController.example); //"/api"
 APIRoutes.get("/todos", apiController.getAllTodos);
 APIRoutes.post("/todos", apiController.createTodo);
 
+app.get("/", (req, res) => {
+    // Get the documents collection
+    const db = client.db(dbName);
+    const collection = db.collection('documents');
+    // Find some documents
+    collection.find({}).toArray(function (err, todos) {
+        assert.equal(err, null);
+        res.render('index', { todos: todos })
+    });
+});
+
 // *********************************
-// Server Listener
+// MongoDB Server Connection
 // *********************************
-app.listen(PORT, () => console.log(`👂Listening on Port ${PORT}👂`));
+
+// Use connect method to connect to the Server
+client.connect(function (err) {
+    assert.equal(null, err);
+
+    // *********************************
+    // Server Listener
+    // *********************************
+    app.listen(PORT, () => console.log(`👂Listening on Port ${PORT}👂`));
+
+    console.log("Connected successfully to database server");
+
+});
+
+
+
